980131922d60529f3f1048eb5556b8b5f6e73225
Mateusz Bogusławski <mateusz.boguslawski@ibnet.pl>
2025-08-17 15:38:34 +0200

    Add transfer size to the protocol

--- src/master/I2CConductor.c
+++ src/master/I2CConductor.c
@@ -13,13 +13,15 @@ void i2c_conductor_init(uint8_t scl, uint8_t sda, i2c_inst_t *i2c) {
 }
 
 int i2c_conductor_write(i2c_instrument_info_t *instrument, uint32_t memory_address, uint8_t *data, uint32_t data_size) {
-	uint32_t message_buff_size = data_size + instrument->memory_address_size;
+	uint32_t message_buff_size = data_size + instrument->memory_address_size * 2;
     uint8_t message_buffer[message_buff_size];
 
 	// Instrument's memory address
 	memcpy(message_buffer, &memory_address, instrument->memory_address_size);
+    // Transfer size
+    memcpy(message_buffer + instrument->memory_address_size, &data_size, instrument->memory_address_size);
     // Accual data
-    memcpy(message_buffer + instrument->memory_address_size, data, data_size);
+    memcpy(message_buffer + instrument->memory_address_size * 2, data, data_size);
 
     return i2c_write_blocking(instrument->i2c, instrument->i2c_address, message_buffer, message_buff_size, false);
 }
--- src/slave/I2CInstrument.c
+++ src/slave/I2CInstrument.c
@@ -4,9 +4,11 @@ struct i2c_context {
     uint8_t *memory;
     uint32_t memory_size;
 	uint32_t memory_address;
+    uint32_t transfer_size;
 	uint32_t bytes_received;
     uint8_t address_size;
     uint8_t address_received : 1;
+    uint8_t size_received : 1;
 };
 
 static volatile struct i2c_context i2c0_context, i2c1_context;
@@ -29,9 +31,18 @@ static void i2c_instrument_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
             context->bytes_received = 0;
         }
 
+        // Set size_received flag if all size bytes were collected (address already received).
+        if ( (context->address_received) && (!context->size_received) && (context->bytes_received == context->address_size) ) {
+            context->size_received = true;
+            context->bytes_received = 0;
+        }
+
         if (!context->address_received) {
 			// Receive memory address (can be multiple bytes long)
             context->memory_address |= (uint32_t)i2c_read_byte_raw(i2c) << (context->bytes_received * 8 );
+            context->bytes_received++;
+        } else if (!context->size_received) {
+            context->transfer_size |= (uint32_t)i2c_read_byte_raw(i2c) << (context->bytes_received * 8 );
             context->bytes_received++;
         } else {
             // Save into memory
@@ -52,7 +63,9 @@ static void i2c_instrument_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
         // Reset all values
         context->memory_address = 0;
 		context->bytes_received = 0;
+        context->transfer_size = 0;
         context->address_received = false;
+        context->size_received = false;
         break;
     
 	default:
@@ -94,5 +107,7 @@ void i2c_instrument_init(uint8_t scl, uint8_t sda, i2c_inst_t *i2c, uint8_t i2c_
 	context->address_size = bytes_needed(memory_size);
 	context->bytes_received = 0;
 	context->memory_address = 0;
+    context->transfer_size = 0;
     context->address_received = false;
+    context->size_received = false;
 }
