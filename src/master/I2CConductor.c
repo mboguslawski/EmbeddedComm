b185142e906e73caa9bce79db80adc45295b8375
Mateusz Bogusławski <mateusz.boguslawski@ibnet.pl>
2025-09-22 23:47:22 +0200

    Convert indentation to tabs.

--- src/master/I2CConductor.c
+++ src/master/I2CConductor.c
@@ -1,75 +1,75 @@
 #include "I2CConductor.h"
 
 void i2c_conductor_init(uint8_t scl, uint8_t sda, i2c_inst_t *i2c) {
-    gpio_init(sda);
-    gpio_set_function(sda, GPIO_FUNC_I2C);
+	gpio_init(sda);
+	gpio_set_function(sda, GPIO_FUNC_I2C);
 	gpio_pull_up(sda);
 
-    gpio_init(scl);
-    gpio_set_function(scl, GPIO_FUNC_I2C);
-    gpio_pull_up(scl);
+	gpio_init(scl);
+	gpio_set_function(scl, GPIO_FUNC_I2C);
+	gpio_pull_up(scl);
 
 	i2c_init(i2c, 1*1000*1000);
 }
 
 int i2c_conductor_write(i2c_instrument_info_t *instrument, uint32_t memory_address, uint8_t *data, uint32_t data_size) {
 	uint32_t message_buff_size = data_size + instrument->memory_address_size * 2 + 1;
-    uint8_t message_buffer[message_buff_size];
+	uint8_t message_buffer[message_buff_size];
 
 	// Instrument's memory address
 	memcpy(message_buffer, &memory_address, instrument->memory_address_size);
-    // Transfer size
-    memcpy(message_buffer + instrument->memory_address_size, &data_size, instrument->memory_address_size);
-    // Accual data
-    memcpy(message_buffer + instrument->memory_address_size * 2, data, data_size);
-    // Calculate checksum and send it as last byte.
-    message_buffer[message_buff_size - 1] = calc_checksum(message_buffer, message_buff_size - 1);
+	// Transfer size
+	memcpy(message_buffer + instrument->memory_address_size, &data_size, instrument->memory_address_size);
+	// Accual data
+	memcpy(message_buffer + instrument->memory_address_size * 2, data, data_size);
+	// Calculate checksum and send it as last byte.
+	message_buffer[message_buff_size - 1] = calc_checksum(message_buffer, message_buff_size - 1);
 
-    return i2c_write_blocking(instrument->i2c, instrument->i2c_address, message_buffer, message_buff_size, false);
+	return i2c_write_blocking(instrument->i2c, instrument->i2c_address, message_buffer, message_buff_size, false);
 }
 
 int i2c_conductor_read(i2c_instrument_info_t *instrument, uint32_t memory_address, uint8_t *data, uint32_t data_size) {
-    // Address and transfer size will be written to i2c slave, both are the same  size.
-    uint32_t write_buffer_size = instrument->memory_address_size * 2;
-    uint8_t write_buffer[write_buffer_size];
+	// Address and transfer size will be written to i2c slave, both are the same  size.
+	uint32_t write_buffer_size = instrument->memory_address_size * 2;
+	uint8_t write_buffer[write_buffer_size];
 
-    // Prepare write buffer
+	// Prepare write buffer
 	memcpy(write_buffer, &memory_address, instrument->memory_address_size);
 	memcpy(write_buffer + instrument->memory_address_size, &data_size, instrument->memory_address_size);
 
-    // Write address and transfer size
-    int res = i2c_write_blocking(instrument->i2c, instrument->i2c_address, write_buffer, write_buffer_size, false);
-    if (res < 0) {
-        return res;
-    }
+	// Write address and transfer size
+	int res = i2c_write_blocking(instrument->i2c, instrument->i2c_address, write_buffer, write_buffer_size, false);
+	if (res < 0) {
+		return res;
+	}
 
-    uint8_t w_checksum = calc_checksum(write_buffer, write_buffer_size);
+	uint8_t w_checksum = calc_checksum(write_buffer, write_buffer_size);
 
-    // Read data from slave into read buffer
-    uint8_t read_buffer[data_size + 1];
-    res = i2c_read_blocking(instrument->i2c, instrument->i2c_address, read_buffer, data_size + 1, false);
-    if (res < 0) {
-        return res;
-    }
+	// Read data from slave into read buffer
+	uint8_t read_buffer[data_size + 1];
+	res = i2c_read_blocking(instrument->i2c, instrument->i2c_address, read_buffer, data_size + 1, false);
+	if (res < 0) {
+		return res;
+	}
 
-    // Transfer data from read buffer only if checksum matches
-    if (calc_checksum_append(read_buffer, data_size, w_checksum) == read_buffer[data_size]) {
-        memcpy(data, read_buffer, data_size);
-    }
+	// Transfer data from read buffer only if checksum matches
+	if (calc_checksum_append(read_buffer, data_size, w_checksum) == read_buffer[data_size]) {
+		memcpy(data, read_buffer, data_size);
+	}
 
-    return data_size;
+	return data_size;
 }
 
 int i2c_conductor_read_instrument_status(i2c_instrument_info_t *instrument, status_register_t *status) {
-    uint8_t read_buffer[2];
-    
-    int ret = i2c_read_blocking(instrument->i2c, instrument->i2c_address, read_buffer, 2, false);
+	uint8_t read_buffer[2];
+	
+	int ret = i2c_read_blocking(instrument->i2c, instrument->i2c_address, read_buffer, 2, false);
 
-    if (calc_checksum_it(0, read_buffer[0]) != read_buffer[1]) {
-        (*status) = I2C_O_ERR_DATA_CORRUPTED;
-    } else {
-        *status = read_buffer[0]; 
-    }
+	if (calc_checksum_it(0, read_buffer[0]) != read_buffer[1]) {
+		(*status) = I2C_O_ERR_DATA_CORRUPTED;
+	} else {
+		*status = read_buffer[0]; 
+	}
 
-    return ret;
+	return ret;
 }
--- src/slave/I2CInstrument.c
+++ src/slave/I2CInstrument.c
@@ -1,17 +1,17 @@
 #include "I2CInstrument.h"
 
 struct i2c_context {
-    uint8_t *memory;
-    uint8_t *write_buffer;
-    volatile uint32_t max_transfer_size;
-    volatile uint32_t memory_size;
+	uint8_t *memory;
+	uint8_t *write_buffer;
+	volatile uint32_t max_transfer_size;
+	volatile uint32_t memory_size;
 	volatile uint32_t memory_address;
-    volatile uint32_t transfer_size;
+	volatile uint32_t transfer_size;
 	volatile uint32_t byte_counter;
-    volatile uint8_t checksum;
-    volatile uint8_t address_size;
-    volatile uint8_t transfer_state;
-    volatile status_register_t status_register;
+	volatile uint8_t checksum;
+	volatile uint8_t address_size;
+	volatile uint8_t transfer_state;
+	volatile status_register_t status_register;
 };
 
 // Possible transfer states
@@ -29,228 +29,228 @@ struct i2c_context {
 static struct i2c_context i2c0_context, i2c1_context;
 
 static inline struct i2c_context *get_context(i2c_inst_t *i2c) {
-    return (i2c == i2c0) ? &i2c0_context : &i2c1_context;
+	return (i2c == i2c0) ? &i2c0_context : &i2c1_context;
 }
 
 // Writes byte to context->memory_address.
 // Sets context->address_received flag, resets context->byte_counter 
 // and sets error flag in status register if all address bytes received.
 static inline void write_memory_address(struct i2c_context *context, uint8_t byte) {
-    // Receive memory address (can be multiple bytes long)
-    context->memory_address |= (uint32_t)byte << (context->byte_counter * 8 );
-    context->byte_counter++;
+	// Receive memory address (can be multiple bytes long)
+	context->memory_address |= (uint32_t)byte << (context->byte_counter * 8 );
+	context->byte_counter++;
 
-    // Move to the next state if all address bytes were collected.
-    if (context->byte_counter == context->address_size) {
-        context->transfer_state = STATE_R_TRANSFER_SIZE;
-        context->byte_counter = 0;
+	// Move to the next state if all address bytes were collected.
+	if (context->byte_counter == context->address_size) {
+		context->transfer_state = STATE_R_TRANSFER_SIZE;
+		context->byte_counter = 0;
 
-        // Check address
-        if (context->memory_address >= context->memory_size) {
-            context->status_register |= I2C_O_ERR_INVALID_ADDR;
-        }
-    }
+		// Check address
+		if (context->memory_address >= context->memory_size) {
+			context->status_register |= I2C_O_ERR_INVALID_ADDR;
+		}
+	}
 }
 
 // Writes byte to context->transfer_size.
 // Sets context->size_received flag, resets context->byte_counter 
 // and sets error flag in status register if all transfer size bytes received.
 static inline void write_transfer_size(struct i2c_context *context, uint8_t byte) {
-    // Receive transfer size (can be multiple bytes long)
-    context->transfer_size |= (uint32_t)byte << (context->byte_counter * 8 );
-    context->byte_counter++;
-    
-    // Move to the next state if all size bytes were collected (address already received).
-    if (context->byte_counter == context->address_size) {
-        context->transfer_state = STATE_R_DATA;
-        context->byte_counter = 0;
+	// Receive transfer size (can be multiple bytes long)
+	context->transfer_size |= (uint32_t)byte << (context->byte_counter * 8 );
+	context->byte_counter++;
+	
+	// Move to the next state if all size bytes were collected (address already received).
+	if (context->byte_counter == context->address_size) {
+		context->transfer_state = STATE_R_DATA;
+		context->byte_counter = 0;
 
-        // Check if declared transfer size does not exceed write_buffer size (if enabled)
-        if ( (context->write_buffer != NULL) && (context->transfer_size > context->max_transfer_size) ) {
-            context->status_register |= I2C_O_ERR_WBUFFER_OVERFLOW;
-        }
+		// Check if declared transfer size does not exceed write_buffer size (if enabled)
+		if ( (context->write_buffer != NULL) && (context->transfer_size > context->max_transfer_size) ) {
+			context->status_register |= I2C_O_ERR_WBUFFER_OVERFLOW;
+		}
 
-        // Check if data will not overflow memory
-        if (context->memory_address + context->transfer_size >= context->memory_size) {
-            context->status_register |= I2C_O_ERR_INVALID_ADDR;
-        }
-    }
+		// Check if data will not overflow memory
+		if (context->memory_address + context->transfer_size >= context->memory_size) {
+			context->status_register |= I2C_O_ERR_INVALID_ADDR;
+		}
+	}
 }
 
 // Writes received byte to memory/write buffer and checks for errors.
 static inline void write_memory(struct i2c_context *context, uint8_t byte) {
-    // Save received byte
-    if (context->write_buffer != NULL) {
-        // To write buffer if enabled
-        context->write_buffer[context->byte_counter] = byte;
-    } else {
-        // Directly to memory, if write buffer disabled
-        context->memory[context->memory_address + context->byte_counter] = byte;
-    }
+	// Save received byte
+	if (context->write_buffer != NULL) {
+		// To write buffer if enabled
+		context->write_buffer[context->byte_counter] = byte;
+	} else {
+		// Directly to memory, if write buffer disabled
+		context->memory[context->memory_address + context->byte_counter] = byte;
+	}
 
-    context->byte_counter++;
+	context->byte_counter++;
 
-    if (context->byte_counter == context->transfer_size) {
-        context->transfer_state = STATE_R_CHECKSUM;
-    }
+	if (context->byte_counter == context->transfer_size) {
+		context->transfer_state = STATE_R_CHECKSUM;
+	}
 }
 
 // Resets context after i2c transfer. Needs to be done before new transfer handling.
 static inline void reset_context(struct i2c_context *context) {
-    
-    // Copy data from write_buffer to memory
-    if ( (context->transfer_state == STATE_T_FINISH) && (context->write_buffer != NULL) && (context->status_register == 0) ) {
-        uint32_t size = MIN(context->max_transfer_size, context->transfer_size);
-        memcpy(context->memory + context->memory_address, context->write_buffer, size);
-    }
+	
+	// Copy data from write_buffer to memory
+	if ( (context->transfer_state == STATE_T_FINISH) && (context->write_buffer != NULL) && (context->status_register == 0) ) {
+		uint32_t size = MIN(context->max_transfer_size, context->transfer_size);
+		memcpy(context->memory + context->memory_address, context->write_buffer, size);
+	}
 
-    // Reset all values
-    context->memory_address = 0;
+	// Reset all values
+	context->memory_address = 0;
 	context->byte_counter = 0;
-    context->transfer_size = 0;
-    context->transfer_state = STATE_IDLE;
-    context->checksum = 0;
+	context->transfer_size = 0;
+	context->transfer_state = STATE_IDLE;
+	context->checksum = 0;
 }
 
 // Compare received checksum with calculated and set error flag in status register if thoose do not match.
 static inline void check_checksum(struct i2c_context *context, uint8_t checksum_byte) {
-    if (context->checksum != checksum_byte) {
-        context->status_register |= I2C_O_ERR_DATA_CORRUPTED;
-    }
+	if (context->checksum != checksum_byte) {
+		context->status_register |= I2C_O_ERR_DATA_CORRUPTED;
+	}
 
-    context->transfer_state = STATE_T_FINISH;
+	context->transfer_state = STATE_T_FINISH;
 }
 
 // Handle all logic related to slave receiving byte from master.
 static inline void write_handler(struct i2c_context *context, uint8_t received_byte) {
-    // Discard byte if any error was detected
-    if (context->status_register != 0) {
-        return;
-    }
+	// Discard byte if any error was detected
+	if (context->status_register != 0) {
+		return;
+	}
 
-    // Include byte in checksum calculation (except received checksum byte).
-    if (context->transfer_state != STATE_R_CHECKSUM) {
-        context->checksum = calc_checksum_it(context->checksum, received_byte);
-    }
+	// Include byte in checksum calculation (except received checksum byte).
+	if (context->transfer_state != STATE_R_CHECKSUM) {
+		context->checksum = calc_checksum_it(context->checksum, received_byte);
+	}
 
-    // Decide where to save receivied byte according to current state.
-    switch (context->transfer_state) {
-    
-    case STATE_IDLE:
-        context->transfer_state = STATE_R_ADDRESS;
-        // No break, execute code to receive address
+	// Decide where to save receivied byte according to current state.
+	switch (context->transfer_state) {
+	
+	case STATE_IDLE:
+		context->transfer_state = STATE_R_ADDRESS;
+		// No break, execute code to receive address
 
-    case STATE_R_ADDRESS:
-        write_memory_address(context, received_byte);
-        break;
+	case STATE_R_ADDRESS:
+		write_memory_address(context, received_byte);
+		break;
 
-    case STATE_R_TRANSFER_SIZE:
-        write_transfer_size(context, received_byte);
-        break;
-    
-    case STATE_R_DATA:
-        write_memory(context, received_byte);
-        break;
+	case STATE_R_TRANSFER_SIZE:
+		write_transfer_size(context, received_byte);
+		break;
+	
+	case STATE_R_DATA:
+		write_memory(context, received_byte);
+		break;
 
-    case STATE_R_CHECKSUM:
-        check_checksum(context, received_byte);
-        break;
-        
-    case STATE_T_FINISH:
-        context->status_register |= I2C_O_ERR_SIZE_MISMATCH;
-        break;
+	case STATE_R_CHECKSUM:
+		check_checksum(context, received_byte);
+		break;
+		
+	case STATE_T_FINISH:
+		context->status_register |= I2C_O_ERR_SIZE_MISMATCH;
+		break;
 
-    default:
-        context->status_register |= I2C_O_ERR_INVALID_FLOW;
-        break;
-    }
+	default:
+		context->status_register |= I2C_O_ERR_INVALID_FLOW;
+		break;
+	}
 }
 
 static inline uint8_t read_status_reg(struct i2c_context *context) {
-    volatile uint8_t *reg_ptr = &context->status_register;
-    uint8_t out_byte = reg_ptr[context->byte_counter];
-    
-    context->checksum = calc_checksum_it(context->checksum, out_byte);
-    context->byte_counter++;
+	volatile uint8_t *reg_ptr = &context->status_register;
+	uint8_t out_byte = reg_ptr[context->byte_counter];
+	
+	context->checksum = calc_checksum_it(context->checksum, out_byte);
+	context->byte_counter++;
 
-    if (context->byte_counter == context->transfer_size) {
-        context->byte_counter = 0;
-        context->transfer_state = STATE_T_CHECKSUM;
-    }
+	if (context->byte_counter == context->transfer_size) {
+		context->byte_counter = 0;
+		context->transfer_state = STATE_T_CHECKSUM;
+	}
 
-    return out_byte;
+	return out_byte;
 }
 
 static inline uint8_t read_memory(struct i2c_context *context) {
-    uint8_t out_byte = context->memory[context->memory_address + context->byte_counter];
+	uint8_t out_byte = context->memory[context->memory_address + context->byte_counter];
 
-    context->checksum = calc_checksum_it(context->checksum, out_byte);
-    context->byte_counter++;
+	context->checksum = calc_checksum_it(context->checksum, out_byte);
+	context->byte_counter++;
 
-    if (context->byte_counter == context->transfer_size) {
-        context->byte_counter = 0;
-        context->transfer_state = STATE_T_CHECKSUM;
-    }
+	if (context->byte_counter == context->transfer_size) {
+		context->byte_counter = 0;
+		context->transfer_state = STATE_T_CHECKSUM;
+	}
 
-    return out_byte;
+	return out_byte;
 }
 
 static inline uint8_t read_checksum(struct i2c_context *context) {
-    context->transfer_state = STATE_R_FINISH;
-    return context->checksum;
+	context->transfer_state = STATE_R_FINISH;
+	return context->checksum;
 }
 
 static inline uint8_t read_handler(struct i2c_context *context) {
-    uint8_t out_byte = 0x0;
+	uint8_t out_byte = 0x0;
 
-    // Send dummy byte if error was detected.
-    if (context->status_register != 0) {
-        return out_byte;
-    }
+	// Send dummy byte if error was detected.
+	if (context->status_register != 0) {
+		return out_byte;
+	}
 
-    switch (context->transfer_state) {
+	switch (context->transfer_state) {
 
-    case STATE_IDLE:
-        context->transfer_state = STATE_T_STATUS;
-        context->transfer_size = 1;
-        // No break, execute reading status register code
+	case STATE_IDLE:
+		context->transfer_state = STATE_T_STATUS;
+		context->transfer_size = 1;
+		// No break, execute reading status register code
 
-    case STATE_T_STATUS:
-        out_byte = read_status_reg(context);
-        break;
-    
-    case STATE_T_DATA:
-        out_byte = read_memory(context);
-        break;
-    
-    case STATE_T_CHECKSUM:
-        out_byte = read_checksum(context);
-        break;
+	case STATE_T_STATUS:
+		out_byte = read_status_reg(context);
+		break;
+	
+	case STATE_T_DATA:
+		out_byte = read_memory(context);
+		break;
+	
+	case STATE_T_CHECKSUM:
+		out_byte = read_checksum(context);
+		break;
 
-    case STATE_R_FINISH:
-        context->status_register |= I2C_O_ERR_SIZE_MISMATCH;
-        break;
+	case STATE_R_FINISH:
+		context->status_register |= I2C_O_ERR_SIZE_MISMATCH;
+		break;
 
-    default:
-        context->status_register |= I2C_O_ERR_INVALID_FLOW;
-        break;
-    }
+	default:
+		context->status_register |= I2C_O_ERR_INVALID_FLOW;
+		break;
+	}
 
-    return out_byte;
+	return out_byte;
 }
 
 static inline void handle_stop(struct i2c_context *context) {
-    if (context->transfer_state == STATE_T_FINISH || context->transfer_state == STATE_R_FINISH) {
-        reset_context(context);
-        return;
-    }
+	if (context->transfer_state == STATE_T_FINISH || context->transfer_state == STATE_R_FINISH) {
+		reset_context(context);
+		return;
+	}
 
-    if (context->transfer_state == STATE_R_DATA && context->byte_counter == 0) {
-        context->transfer_state = STATE_T_DATA;
-        return;
-    }
+	if (context->transfer_state == STATE_R_DATA && context->byte_counter == 0) {
+		context->transfer_state = STATE_T_DATA;
+		return;
+	}
 
-    context->status_register |= I2C_O_ERR_INVALID_FLOW;
+	context->status_register |= I2C_O_ERR_INVALID_FLOW;
 }
 
 // Handles all i2c communication logic on slave side.
@@ -261,75 +261,75 @@ static void i2c_instrument_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
 	switch (event) {
 	
 	// Master has written some data
-    case I2C_SLAVE_RECEIVE:
-        uint8_t in_byte = i2c_read_byte_raw(i2c);
-        write_handler(context, in_byte);
-        break;
+	case I2C_SLAVE_RECEIVE:
+		uint8_t in_byte = i2c_read_byte_raw(i2c);
+		write_handler(context, in_byte);
+		break;
 	
 	// Master is requesting data
-    case I2C_SLAVE_REQUEST:
-        i2c_write_byte_raw(i2c, read_handler(context));
-        break;
+	case I2C_SLAVE_REQUEST:
+		i2c_write_byte_raw(i2c, read_handler(context));
+		break;
 	
 	// Master has signalled Stop or Restart
 	case I2C_SLAVE_FINISH:
-        handle_stop(context);
-        break;
+		handle_stop(context);
+		break;
 
 	default:
-        break;
-    }
+		break;
+	}
 }
 
 void i2c_instrument_enable_write_buffer(i2c_inst_t *i2c, uint8_t *memory, uint32_t memory_size) {
-    volatile struct i2c_context* context = get_context(i2c);
+	volatile struct i2c_context* context = get_context(i2c);
 
-    context->write_buffer = memory;
-    context->max_transfer_size = memory_size;
+	context->write_buffer = memory;
+	context->max_transfer_size = memory_size;
 }
 
 status_register_t i2c_instrument_get_status(i2c_inst_t *i2c) {
-    return get_context(i2c)->status_register;
+	return get_context(i2c)->status_register;
 }
 
 inline uint8_t bytes_needed(uint64_t n) {
-    uint8_t result = 1; // at least 1 byte
-    result += (n > 0xFF); // needs >1 byte
-    result += (n > 0xFFFF); // needs >2 bytes
-    result += (n > 0xFFFFFF); // needs >3 bytes
-    result += (n > 0xFFFFFFFF); // needs >4 bytes
-    result += (n > 0xFFFFFFFFFFULL); // needs >5 bytes
-    result += (n > 0xFFFFFFFFFFFFULL); // needs >6 bytes
-    result += (n > 0xFFFFFFFFFFFFFFULL); // needs >7 bytes
-    return result;
+	uint8_t result = 1; // at least 1 byte
+	result += (n > 0xFF); // needs >1 byte
+	result += (n > 0xFFFF); // needs >2 bytes
+	result += (n > 0xFFFFFF); // needs >3 bytes
+	result += (n > 0xFFFFFFFF); // needs >4 bytes
+	result += (n > 0xFFFFFFFFFFULL); // needs >5 bytes
+	result += (n > 0xFFFFFFFFFFFFULL); // needs >6 bytes
+	result += (n > 0xFFFFFFFFFFFFFFULL); // needs >7 bytes
+	return result;
 }
 
 void i2c_instrument_init(uint8_t scl, uint8_t sda, i2c_inst_t *i2c, uint8_t i2c_address, uint8_t *memory, uint32_t memory_size) {
 	// Initialize SDA pin
 	gpio_init(sda);
-    gpio_set_function(sda, GPIO_FUNC_I2C);
-    gpio_pull_up(sda);
+	gpio_set_function(sda, GPIO_FUNC_I2C);
+	gpio_pull_up(sda);
 
 	// Initialize SCL pin
-    gpio_init(scl);
-    gpio_set_function(scl, GPIO_FUNC_I2C);
-    gpio_pull_up(scl);
+	gpio_init(scl);
+	gpio_set_function(scl, GPIO_FUNC_I2C);
+	gpio_pull_up(scl);
 
 	// Initialize i2c with 1MHz frequency
-    i2c_init(i2c, 1*1000*1000);
-    i2c_slave_init(i2c, i2c_address, &i2c_instrument_handler);
+	i2c_init(i2c, 1*1000*1000);
+	i2c_slave_init(i2c, i2c_address, &i2c_instrument_handler);
 
 	// Assign values to context that corresponds to i2c instance
 	struct i2c_context* context = get_context(i2c);
 	context->memory = memory;
 	context->memory_size = memory_size;
-    context->write_buffer = NULL;
-    context->max_transfer_size = 0;
+	context->write_buffer = NULL;
+	context->max_transfer_size = 0;
 	context->address_size = bytes_needed(memory_size);
 	context->byte_counter = 0;
 	context->memory_address = 0;
-    context->transfer_size = 0;
-    context->transfer_state = STATE_IDLE;
-    context->status_register = 0;
-    context->checksum = 0;
+	context->transfer_size = 0;
+	context->transfer_state = STATE_IDLE;
+	context->status_register = 0;
+	context->checksum = 0;
 }
