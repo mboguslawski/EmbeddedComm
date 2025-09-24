e802dfe94ed745aca9c67ca53969defc53aeedc0
Mateusz Bogusławski <mateusz.boguslawski@ibnet.pl>
2025-09-24 16:47:41 +0200

    Remove #include "i2c_orchestra_checksum.h" lines

--- src/slave/I2CInstrument.h
+++ src/slave/I2CInstrument.h
@@ -7,7 +7,6 @@
 #include <string.h>
 
 #include "i2c_orchestra_errors.h"
-#include "i2c_orchestra_checksum.h"
 
 void i2c_instrument_init(uint8_t scl, uint8_t sda, i2c_inst_t *i2c, uint8_t i2c_address, uint8_t *memory, uint32_t memory_size, uint8_t *mem_buffer, uint32_t mem_buffer_size);
 uint8_t i2c_instrument_get_status(i2c_inst_t *i2c);
