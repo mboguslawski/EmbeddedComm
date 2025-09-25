6c7f5360117ba803d5248154b1880db9ffcb0337
Mateusz Bogusławski <mateusz.boguslawski@ibnet.pl>
2025-09-25 22:09:51 +0200

    Convert indentation to tabs

--- examples/picoMasterI2C/picoMasterI2C.cpp
+++ examples/picoMasterI2C/picoMasterI2C.cpp
@@ -8,46 +8,46 @@
 #define I2C_SCL 15
 
 int main() {
-    stdio_init_all();
-    sleep_ms(4000);
-        
-    puts("Pico I2C master example\n");
-    
-    picoMasterI2C master(I2C_SCL, I2C_SDA, I2C_PORT);
-    uint8_t slaveAddress = 0x17;
+	stdio_init_all();
+	sleep_ms(4000);
+		
+	puts("Pico I2C master example\n");
+	
+	picoMasterI2C master(I2C_SCL, I2C_SDA, I2C_PORT);
+	uint8_t slaveAddress = 0x17;
 
-    uint8_t buffer[16] = {};
-    uint8_t status;
-    uint8_t slave_tchecksum;
-    
-    int i = 0;
-    while (true) {
-        printf("%d\n", i);
-        i++;
-        
-        for (uint32_t i = 0; i < 16; i++) {
-            buffer[i] += 1;
-        }
+	uint8_t buffer[16] = {};
+	uint8_t status;
+	uint8_t slave_tchecksum;
+	
+	int i = 0;
+	while (true) {
+		printf("%d\n", i);
+		i++;
+		
+		for (uint32_t i = 0; i < 16; i++) {
+			buffer[i] += 1;
+		}
 
-        master.write(slaveAddress, 255-16, buffer, 16);
-        master.readStatus(slaveAddress, &status);
-        printf("Write status %u\n", status);
+		master.write(slaveAddress, 255-16, buffer, 16);
+		master.readStatus(slaveAddress, &status);
+		printf("Write status %u\n", status);
 
 
-        for (uint32_t i = 0; i < 16; i++) {
-            buffer[i] = 0;
-        }
+		for (uint32_t i = 0; i < 16; i++) {
+			buffer[i] = 0;
+		}
 
-        master.read(slaveAddress, 255-16, buffer, 16);
-        master.readTChecksum(slaveAddress, &slave_tchecksum);
-        master.readStatus(slaveAddress, &status);
-        printf("Read status %u\n", status);
+		master.read(slaveAddress, 255-16, buffer, 16);
+		master.readTChecksum(slaveAddress, &slave_tchecksum);
+		master.readStatus(slaveAddress, &status);
+		printf("Read status %u\n", status);
 
-        printf("Data:\n");
-        for (uint32_t i = 0; i < 16; i++) {
-            printf("%u %u\n", i, buffer[i]);
-        }
+		printf("Data:\n");
+		for (uint32_t i = 0; i < 16; i++) {
+			printf("%u %u\n", i, buffer[i]);
+		}
 
-        sleep_ms(1000);
-    }
+		sleep_ms(1000);
+	}
 }
--- examples/picoSlaveI2C/picoSlave.cpp
+++ examples/picoSlaveI2C/picoSlave.cpp
@@ -12,11 +12,11 @@ uint8_t memory[256];
 uint8_t buffer[64];
 
 int main() {
-    stdio_init_all();
+	stdio_init_all();
 
-    initPicoSlaveI2C(SCL, SDA, i2c0, I2C_SLAVE_ADDRESS, memory, 256, buffer, 64);
+	initPicoSlaveI2C(SCL, SDA, i2c0, I2C_SLAVE_ADDRESS, memory, 256, buffer, 64);
 
-    while (true) {
-      tight_loop_contents();
-    }
+	while (true) {
+		tight_loop_contents();
+	}
 }
