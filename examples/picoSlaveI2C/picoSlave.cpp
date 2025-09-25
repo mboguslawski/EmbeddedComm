1790eb03ca44b4c24a44c05e22b640d971264834
Mateusz Bogusławski <mateusz.boguslawski@ibnet.pl>
2025-09-25 19:02:23 +0200

    Add picoSlaveI2C usage example

---
+++ examples/picoSlaveI2C/picoSlave.cpp
@@ -0,0 +1,22 @@
+#include <pico/stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "picoSlaveI2C.h"
+
+static const uint I2C_SLAVE_ADDRESS = 0x17;
+static const uint SDA = 16;
+static const uint SCL = 17;
+
+uint8_t memory[256];
+uint8_t buffer[64];
+
+int main() {
+    stdio_init_all();
+
+    initPicoSlaveI2C(SCL, SDA, i2c0, I2C_SLAVE_ADDRESS, memory, 256, buffer, 64);
+
+    while (true) {
+      tight_loop_contents();
+    }
+}
