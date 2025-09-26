98076bdaf56b2b035dfe3701cccc3c3290c84026
Mateusz Bogusławski <mateusz.boguslawski@ibnet.pl>
2025-09-26 17:03:25 +0200

    Add license header with author to all source files

--- examples/picoSlaveI2C/picoSlave.cpp
+++ examples/picoSlaveI2C/picoSlave.cpp
@@ -1,9 +1,22 @@
 /*
- * Copyright (c) 2021 Valentin Milea <valentin.milea@gmail.com>
- * Copyright (c) 2023 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
+picoSlaveI2C.cpp
+
+Example usage of picoSlaveI2C class
+Uses pico's i2c0 port with pin 16 as sda and pin 17 as scl.
+
+Copyright (C) 2025 Mateusz Bogusławski, E: mateusz.boguslawski@ibnet.pl
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/.
+*/
 
 #include <pico/stdlib.h>
 #include <stdio.h>
--- src/master/picoMasterI2C/picoMasterI2C.hpp
+++ src/master/picoMasterI2C/picoMasterI2C.hpp
@@ -1,3 +1,23 @@
+/*
+picoMasterI2C.hpp
+
+Implementation of GenericMaster class for raspberry pi pico (rp2040 and rp2350),
+with I2C protocol.
+
+Copyright (C) 2025 Mateusz Bogusławski, E: mateusz.boguslawski@ibnet.pl
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/.
+*/
+
 #pragma once
 
 #include <hardware/i2c.h>
